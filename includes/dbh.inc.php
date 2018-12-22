d693423f9dae96f1fcd8a3065643aa47b3fb3e5e
Erick Huaman <ebrianh23@gmail.com>
2018-12-22 16:59:00 -0500

    Conexion php a bd test

---
+++ includes/dbh.inc.php
@@ -0,0 +1,9 @@
+<?php
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = "likamos123";
+$dbName = "test";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
+?>
