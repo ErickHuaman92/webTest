55ee19eade4cf6496a91d88255df842bdd6d5974
Erick Huaman <ebrianh23@gmail.com>
2018-12-23 00:10:12 -0500

    Cambios en index.php - mostrar datos de bd

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -6,4 +6,6 @@ $dbPassword = "likamos123";
 $dbName = "test";
 
 $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
+//ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'contraseña';
+
+
--- index.php
+++ index.php
@@ -14,7 +14,17 @@
 </head>
 <body>
 
- 
+ <?php
+    $sql = "select * from Producto;";
+    $result = mysqli_query($conn, $sql);
+    $resultCheck = mysqli_num_rows($result);
+    
+    if ($resultCheck > 0){
+        while ($row = mysqli_fetch_assoc($result)){
+            echo $row['nombre'] . "<br>";
+        }
+    }
+ ?>
 
 </body>
 </html>>
